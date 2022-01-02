364d7cb821828e5786a7e3bc2d131a27a42a3142
naina-coder <82169500+naina-coder@users.noreply.github.com>
2022-01-02 09:30:06 +0530

    Add files via upload

--- Main.js
+++ Main.js
@@ -7,15 +7,18 @@
 
  function setup()
  {
-    createCanvas(900,900);
+    canvas = createCanvas(1000,900);
     video = createCapture(VIDEO);
+   canvas.position(150,200)
     video.hide();
-    console.log("working")
+    console.log("working");
  }
  
 
 function draw()
 {
+    image(video,500,80,300,250);
+
     fill('#57472c');
     stroke('#57472c');
     rect(150, 100, 2, 118);
@@ -34,18 +37,24 @@ function draw()
 
     fill('#57472c');
     stroke('#57472c');
-    rect(800, 100, 2, 180);
+    rect(855, 100, 2, 180);
 
     fill('#0ddb8c');
     stroke('#41db0d');
-    ellipse(800, 75, 85, 100);
+    ellipse(855, 75, 85, 100);
 
 
     fill('#57472c');
     stroke('#57472c');
-    rect(700, 100, 2, 118);
+    rect(960, 100, 2, 118);
 
     fill('#4c147a');
     stroke('#4c147a');
-    ellipse(700, 75, 85, 100);
+    ellipse(960, 75, 85, 100);
+}
+
+
+function take_snapShot()
+{
+    save("Hbday.png");
 }
